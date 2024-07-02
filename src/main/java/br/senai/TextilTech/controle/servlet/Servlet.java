68fc05d88098795dc06180ffbc294814939983a0
EduardoHessmann <eduardo.h.rohleder@gmail.com>
2024-07-02 20:04:01 -0300

    Criação do servlet e inicio dos métodos de inserir funcionario e norma

--- src/main/java/br/senai/TextilTech/controle/servlet/Servlet.java
+++ src/main/java/br/senai/TextilTech/controle/servlet/Servlet.java
@@ -2,6 +2,7 @@ package br.senai.TextilTech.controle.servlet;
 
 import java.io.IOException;
 import java.sql.SQLException;
+import java.time.LocalDate;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -9,20 +10,38 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
+import br.senai.TextilTech.modelo.dao.funcao.FuncaoDAO;
+import br.senai.TextilTech.modelo.dao.funcao.FuncaoDAOImpl;
+import br.senai.TextilTech.modelo.dao.funcionario.FuncionarioDAO;
+import br.senai.TextilTech.modelo.dao.funcionario.FuncionarioDAOImpl;
+import br.senai.TextilTech.modelo.dao.maquina.MaquinaDAO;
+import br.senai.TextilTech.modelo.dao.maquina.MaquinaDAOImpl;
+import br.senai.TextilTech.modelo.dao.norma.NormaDAO;
+import br.senai.TextilTech.modelo.dao.norma.NormaDAOImpl;
 import br.senai.TextilTech.modelo.dao.usuario.UsuarioDAO;
 import br.senai.TextilTech.modelo.dao.usuario.UsuarioDAOImpl;
+import br.senai.TextilTech.modelo.entidade.norma.Norma;
+import br.senai.TextilTech.modelo.entidade.usuario.funcionario.Funcionario;
 
 @WebServlet("/")
 public class Servlet extends HttpServlet {
 
-	private static final long serialVersionUID = 8840029940617992062L;
+	private static final long serialVersionUID = 4085698799982778747L;
 
 	private UsuarioDAO usuarioDAO;
-
+	private FuncaoDAO funcaoDAO;
+	private FuncionarioDAO funcionarioDAO;
+	private MaquinaDAO maquinaDAO;
+	private NormaDAO normaDAO;
 
 	public void init() {
 		usuarioDAO = new UsuarioDAOImpl();
+		funcaoDAO = new FuncaoDAOImpl();
+		funcionarioDAO = new FuncionarioDAOImpl();
+		maquinaDAO = new MaquinaDAOImpl();
+		normaDAO = new NormaDAOImpl();
 
 	}
 
@@ -72,6 +91,55 @@ public class Servlet extends HttpServlet {
 		dispatcher.forward(request, response);
 	}
 
-	
+//	private void logar(HttpServletRequest request, HttpServletResponse response)
+//			throws SQLException, IOException, ServletException {
+//
+//		String email = request.getParameter("email");
+//		String senha = request.getParameter("senha");
+//		HttpSession sessao = request.getSession();
+//		Usuario usuario = usuarioDAO.buscarUsuarioPorEmailESenha(email, senha);
+//		sessao.setAttribute("usuario", usuario);
+//
+//		
+//	}
 
+	private void deslogar(HttpServletRequest request, HttpServletResponse response)
+			throws SQLException, IOException, ServletException {
+
+		HttpSession sessao = request.getSession();
+		sessao.invalidate();
+		response.sendRedirect("");
+	}
+
+	private void inserirColetor(HttpServletRequest request, HttpServletResponse response)
+			throws SQLException, IOException, ServletException {
+
+		String nome = request.getParameter("nome");
+		String cargo = request.getParameter("cargo");
+		String departamento = request.getParameter("departamento");
+		Double salario = Double.parseDouble(request.getParameter("salario"));
+		String horarioFuncionamento = request.getParameter("horarioFuncionamento");
+		String senha = request.getParameter("senha");
+
+		usuarioDAO.inserirUsuario(new Funcionario(nome, senha, cargo, departamento, salario, horarioFuncionamento));
+
+		response.sendRedirect("");
+
+	}
+
+	private void inserirNorma(HttpServletRequest request, HttpServletResponse response)
+			throws SQLException, IOException, ServletException {
+
+		String tipo = request.getParameter("tipo");
+		String descricao = request.getParameter("descricao");
+		LocalDate dataAberturaNorma = LocalDate.parse(request.getParameter("dataAberturaNorma"));
+		LocalDate dataEdicaoNorma = LocalDate.parse(request.getParameter("dataEdicaoNorma"));
+		LocalDate dataRevisaoNorma = LocalDate.parse(request.getParameter("dataRevisaoNorma"));
+		String homologacao = request.getParameter("homologacao");
+
+		normaDAO.inserirNorma(new Norma(tipo, descricao, dataAberturaNorma, dataEdicaoNorma, dataRevisaoNorma, homologacao));
+		
+		response.sendRedirect("");
+
+	}
 }
